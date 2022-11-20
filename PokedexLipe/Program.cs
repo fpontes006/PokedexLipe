3790776ae182e19a31745da3bd7491a9cf033f32
fpontes006 <fpontes006@gmail.com>
2022-11-20 19:16:10 -0300

    Adicionar arquivos de projeto.

---
+++ PokedexLipe/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//1 - salvar imagem
+
+var url = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+var path = @"C:\Users\fpont\OneDrive\Área de Trabalho\Felipe\Pokemons\";
+HttpClient HttpClient = new HttpClient();
+
+for (int i = 1; i <= 999; i++)
+{
+
+    await SalvarImg(i);
+}
+
+async Task SalvarImg(int numero)
+{
+    var idPokemon = numero.ToString().PadLeft(3, '0');
+    var fullUrl = url + idPokemon + ".png";
+    var response = await HttpClient.GetAsync(fullUrl);
+    using (var fs = new FileStream(path + idPokemon + ".png", FileMode.CreateNew))
+    {
+        var result = response.Content.CopyToAsync(fs);
+        result.Wait();
+    }
+
+}
